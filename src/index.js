0910c5f6a42f30f21dc8f63327bb0bab8ad7ce8c
Edison Kurt Bernstein Iriart <kiriart@yahoo.com>
2022-12-05 14:45:59 +1100

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // console.log(event.target.children[1].value);
+
+    // fetch(`http://localhost:3000/movies/${event.target.children[1].value}`)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
+
+    const input = document.querySelector("input#searchByID");
+    // console.log(input.value);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        console.log("error5");
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      })
+      .catch((err) => {
+        console.log("error");
+        const title = document.querySelector("section#movieDetails h4");
+        title.innerText = "Movie does not exist!";
+        console.log(title.innerText);
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
